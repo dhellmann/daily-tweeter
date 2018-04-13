1acff44eccdad6a1aeca042265357e134f0a98ce
Doug Hellmann <doug@doughellmann.com>
2018-04-13 18:54:54 -0400

    read post file and publish post
    Make an attempt to republish using a different string by adding a
    prefix if we get an error indicating that a message has already been
    posted.

--- daily_tweeter/cmd.py
+++ daily_tweeter/cmd.py
@@ -1,12 +1,17 @@
 import argparse
+import datetime
 import logging
 import os.path
 import sys
 
-import appdirs
-
 from daily_tweeter import client
 from daily_tweeter import config
+from daily_tweeter import posts
+
+import appdirs
+import tweepy
+
+LOG = logging.getLogger(__name__)
 
 
 def main():
@@ -21,11 +26,20 @@ def main():
         help='location of configuration file',
     )
     parser.add_argument(
+        '--repost-prefix',
+        default='Reposting:',
+        help='prefix for reposting',
+    )
+    parser.add_argument(
         '-v',
         action='store_true',
         dest='verbose',
         default=False,
         help='turn on verbose output',
+    )
+    parser.add_argument(
+        'post_file',
+        help='location of YAML file containing posts',
     )
     args = parser.parse_args()
 
@@ -44,3 +58,24 @@ def main():
         twitter = client.get_client(cfg)
     except Exception as e:
         parser.error(e)
+
+    post_data = posts.load_posts(args.post_file)
+    today = str(datetime.date.today())
+    print(post_data, today)
+    to_post = post_data['by-date'].get(today)
+    if not to_post:
+        LOG.debug('no post scheduled for %s', today)
+    else:
+        LOG.debug('posting %r', to_post)
+        try:
+            twitter.update_status(status=to_post)
+        except tweepy.error.TweepError as e:
+            if e.api_code == 187:
+                # Duplicate
+                to_post = '{} {}'.format(
+                    args.repost_prefix,
+                    to_post,
+                )
+                twitter.update_status(status=to_post)
+            else:
+                raise
---
+++ daily_tweeter/posts.py
@@ -0,0 +1,23 @@
+import logging
+
+import yaml
+
+LOG = logging.getLogger(__name__)
+
+
+def load_posts(filename):
+    LOG.debug('reading post data from %s', filename)
+    with open(filename, 'r', encoding='utf-8') as f:
+        data = yaml.safe_load(f.read())
+    if data is None:
+        data = {}
+    posts = data.get('posts', [])
+    LOG.debug('found %d entries in post section',
+              len(posts))
+    # Build the by-date version of the data structure for quick
+    # lookup.
+    data['by-date'] = {
+        p['date']: p['message']
+        for p in posts
+    }
+    return data
